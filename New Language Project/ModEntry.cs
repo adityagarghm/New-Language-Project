acb71b2e82649f7745c1a5cb683e80f56067b104
Aditya <aditya_garg@horacemann.org>
2026-05-07 09:11:47 -0400

    Initial SMAPI mod setup

---
+++ New Language Project/ModEntry.cs
@@ -0,0 +1,20 @@
+﻿using StardewModdingAPI;
+
+namespace NewLanguageProject
+{
+    public class ModEntry : Mod
+    {
+        public override void Entry(IModHelper helper)
+        {
+            Monitor.Log("Mod loaded successfully!", LogLevel.Info);
+
+            // test hook
+            helper.Events.GameLoop.DayStarted += OnDayStarted;
+        }
+
+        private void OnDayStarted(object? sender, StardewModdingAPI.Events.DayStartedEventArgs e)
+        {
+            Monitor.Log("A new day started!", LogLevel.Info);
+        }
+    }
+}
